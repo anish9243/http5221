d18c89437de120c58ddf4fce96f58d5aec6139de
Anish <administrator@Anishs-MacBook-Pro.local>
2024-06-12 11:05:25 -0400

    Assignment 1

---
+++ Assignment 1/assign1.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+  var formHandle = document.forms.login__form;
+  var username = formHandle.username;
+  var password = formHandle.password;
+  var output = document.getElementById("output_box");
+  formHandle.onsubmit = processForm;
+
+  function processForm() {
+    var loginValidation = checkLogin(username.value, password.value);
+    output.style.display = "block";
+    if (loginValidation === true) {
+      output.innerHTML = "Welcome Back!";
+    } else {
+      output.innerHTML = loginValidation;
+    }
+    return false;
+  }
+
+  /**
+   * Function to validate login credentials.
+   * @param {string} username
+   * @param {string} password
+   * @returns {boolean|string} true if credentials are valid, otherwise an error message
+   */
+  function checkLogin(username, password) {
+    // console.log("user:", username, "Pass:", password); Checking username and password
+    const validUsername = "anish098";
+    const validPassword = "87757270c86d517698777aeba4273838";
+    const encryptedPassword = md5Encrypt(password);
+    console.log(encryptedPassword);
+    if (!username) {
+      return "No Username Entered.";
+    } else if (!password) {
+      return "No Password Entered.";
+    } else if (username !== validUsername || encryptedPassword !== validPassword) {
+      return "Invalid Username or Password.";
+    } else {
+      return true;
+    }
+  }
+};
